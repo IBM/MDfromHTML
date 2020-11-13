1401b3ce4c788422b8bf5ee9bede4d26d63a68f7
Nathaniel Mills <wnm3@us.ibm.com>
2020-11-13 16:44:38 -0500

    IBM/MDfromHTML

--- loadMDfromHTMLWebServices.sh
+++ loadMDfromHTMLWebServices.sh
@@ -10,7 +10,7 @@ if [ ! -d "/store/WAAData/mdfromhtmlsvcs/properties" ]
 then
    docker run --publish 9081:9081 --detach --name mdfromhtmlsvcs mdfromhtmlwebservices
 else
-   docker run -v /store/WAALogs:/logs -v /store/WAAData/mdfromhtml/properties/:/opt/ol/wlp/output/defaultServer/properties -v /store/WAAData/mdfromhtml/data/:/opt/ol/wlp/output/defaultServer/data --publish 9081:9081 --detach --name mdfromhtmlsvcs mdfromhtmlwebservices
+   docker run -v /store/WAALogs:/logs -v /store/WAAData/mdfromhtmlsvcs/properties/:/opt/ol/wlp/output/defaultServer/properties -v /store/WAAData/mdfromhtmlsvcs/data/:/opt/ol/wlp/output/defaultServer/data --publish 9081:9081 --detach --name mdfromhtmlsvcs mdfromhtmlwebservices
 fi
 sleep 10
 docker logs mdfromhtmlsvcs
